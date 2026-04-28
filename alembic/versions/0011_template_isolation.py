1733998fa8aab2a296cc6a9d826a1aabf3d6a24b
Dorian Przetakiewicz <dorian.przetakiewicz@gmail.com>
2026-04-28 12:57:27 +0200

    migration file rename

--- alembic/versions/0011_template_isolation.py
+++ alembic/versions/0011_template_isolation.py
@@ -1,7 +1,7 @@
 """add user_id columns to pipeline_templates and prompt_templates
 
-Revision ID: 0008
-Revises: 0007
+Revision ID: 0011
+Revises: 0010
 Create Date: 2026-04-28
 
 """
@@ -12,8 +12,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "0008"
-down_revision: Union[str, None] = "0007"
+revision: str = "0011"
+down_revision: Union[str, None] = "0010"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
